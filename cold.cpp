8e3ce2068350717b844e32d9e285784029b45c73
homarcus <60651050+homarcus@users.noreply.github.com>
2020-04-02 07:10:52 +0100

    Create cold.cpp

---
+++ cold.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int n;
+    cin >> n;
+
+    int temp;
+    int count = 0;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> temp;
+        if (temp < 0)
+        {
+            count++;
+        }
+    }
+
+    cout << count;
+
+    return 0;
+}
