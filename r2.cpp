4816195098a9a725a40df76c78ed07905496cfec
homarcus <60651050+homarcus@users.noreply.github.com>
2020-04-02 07:22:54 +0100

    Create r2.cpp

---
+++ r2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int r1{};
+    int mean{};
+    cin >> r1 >> mean;
+    int r2{};
+    r2 = (2 * mean) - r1;
+    cout << r2;
+
+    return 0;
+}
