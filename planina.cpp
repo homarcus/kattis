65277b1815bce0ae3a331c4dee1b9ae32d86a857
homarcus <60651050+homarcus@users.noreply.github.com>
2020-04-02 07:21:21 +0100

    Create planina.cpp

---
+++ planina.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h> 
+using namespace std; 
+int main() 
+{ 
+    int x;
+    cin >> x;
+    x = (int)(pow(2, x) + 1);
+    x = (int)(pow(x, 2));
+    cout << x; 
+  
+    return 0; 
+} 
