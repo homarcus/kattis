61cdec282d7fbd4187761d138a323c6f499e6676
homarcus <60651050+homarcus@users.noreply.github.com>
2020-04-02 07:22:10 +0100

    Create timeloop.cpp

---
+++ timeloop.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main(){
+
+int y{};
+   cin >> y;
+   int new_array[y];
+
+   for(int i = 1;i <= y; i++){
+
+      cout<< i << " Abracadabra "<<endl;
+
+   }
+   return 0;
+}
