01b23faea117642e97f40753bc2f7c07a1a3eebd
homarcus <60651050+homarcus@users.noreply.github.com>
2020-04-02 07:21:48 +0100

    Create twostones.cpp

---
+++ twostones.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main(){
+
+    int input;
+    cin >> input;
+    if (input % 2==0)
+    {
+        cout << "Bob";
+    }else{
+        cout << "Alice";
+    }
+
+   return 0;
+}
